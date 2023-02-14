710e1e54d43e6aeb68fb90586d1b527b0e336cc5
muffindud <ccatlabuga2002@gmail.com>
2023-02-14 13:52:50 +0200

    add string generation

--- Grammar.py
+++ Grammar.py
@@ -18,23 +18,40 @@ class Grammar:
         self.p = p
         self.s = s
 
+        for key in self.p:
+            self.p[key].sort(key=lambda st: len(st))
+            for s in self.p[key]:
+                if key in s:
+                    for i in range(len(self.p[key])):
+                        if self.p[key] == s and i < len(self.p[key]) - 1:
+                            self.p[key].pop(i)
+                            self.p[key].append(s)
+
     # Check if a string is valid (terminal)
     def check(self, s):
         return not any(c not in self.vt for c in s)
 
     def generate_string(self):
-        passes = 0
         strings = []
         final_strings = []
         string = self.s
         strings.append(string)
-        while len(final_strings) < 5 and passes <= 100:
-            passes += 1 # Prevent infinite loop
+        while len(final_strings) < 5:
+            for c in string:
+                if c in self.vn:
+                    ind = 0
+                    initial_string = string
+                    string = string.replace(c, self.p[c][ind], 1)
+                    if string in final_strings:
+                        string = initial_string
+                        ind += 1
+                        string = string.replace(c, self.p[c][ind], 1)
 
-
+                    if not self.check(string):
+                        strings.append(string)
 
             if self.check(string) and string not in final_strings:
                 final_strings.append(string)
                 string = strings[-1]
 
-        return final_strings, strings
+        return final_strings
